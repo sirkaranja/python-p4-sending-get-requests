a987ba457c5b75859158de519de6207d5c84bc04
daniel <sirkaranja12@gmail.com>
2023-06-25 16:42:10 +0300

    get requests

--- lib/get.py
+++ lib/get.py
@@ -3,7 +3,7 @@ import json
 
 url = "https://learn-co-curriculum.github.io/json-site-example/endpoints/locations.json"
 
-response = requests.get(url)
+response = requests.get(url) #get request to display courses in the above url
 
 json_content = json.loads(response.text)
 
